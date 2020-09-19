9544605fb84d246205e1c1647cfec62d005ef2d5
luciano-jr <luciano@lucianojr.com.br>
2020-09-19 14:35:16 +0100

    Fixed issue

--- App.js
+++ App.js
@@ -3,41 +3,9 @@ import { AppLoading } from 'expo';
 import { Container, Text } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
-import AnatomyExample from './src/AnatomyExample'
-import SideBar from './src/SideBar';
-import { createDrawerNavigator, createStackNavigator } from "@react-navigation/drawer";
-
-const Drawer = createDrawerNavigator(
-  {
-    Anatomy: { screen: AnatomyExample },
-  },
-  {
-    initialRouteName: "Anatomy",
-    contentOptions: {
-      activeTintColor: "#e91e63"
-    },
-    contentComponent: props => <SideBar {...props} />
-  }
-);
-
-const AppNavigator = createStackNavigator(
-  {
-    Drawer: { screen: Drawer },
-  },
-  {
-    initialRouteName: "Drawer",
-    headerMode: "none"
-  }
-);
+import NavigationExample  from './src/NavigationExample'
 
 export default class App extends React.Component {
-  closeDrawer() {
-    this.drawer._root.close()
-  };
-  openDrawer() {
-    this.drawer._root.open()
-  };
-
   constructor(props) {
     super(props);
     this.state = {
@@ -61,13 +29,7 @@ export default class App extends React.Component {
 
     return (
       <Container>
-        <Drawer
-          ref={(ref) => { this.drawer = ref; }}
-          content={<SideBar navigator={this.navigator} />}
-          onClose={() => this.closeDrawer()} >
-          <AnatomyExample />
-        </Drawer>
-
+        <NavigationExample />
       </Container>
     );
   }
