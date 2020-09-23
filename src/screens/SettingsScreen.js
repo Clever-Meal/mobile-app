bba1138741289c01cb2330c856b9e824899622ac
luciano-jr <luciano@lucianojr.com.br>
2020-09-23 18:38:06 +0100

    Added explore screen content

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -4,18 +4,18 @@ import { Container, Content } from 'native-base';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 
-const MoreScreen = ({
+const SettingsScreen = ({
     params,
 }) => (
     <Container>
         <Header title="Settings" />
         <Content>
             <View>
-                <Text>MoreScreen </Text>
+                <Text>SettingsScreen </Text>
             </View>
         </Content>
         <Footer />
     </Container>
 );
 
-export default MoreScreen;
+export default SettingsScreen;
