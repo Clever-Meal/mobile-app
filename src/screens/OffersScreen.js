48d95664cd2517d9508244d7b401db290bc07b41
joao.deluchi <joao.deluchi@hbsis.com.br>
2020-09-28 20:56:57 -0300

    scanning and storage in the component state, it remains to activate the flash

--- src/screens/OffersScreen.js
+++ src/screens/OffersScreen.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Container, Content } from 'native-base';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Container, Button } from 'native-base';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-const OffersScreen = ({
-    params,
-}) => (
-    <Container>
-        <Header title="Offers" />
-        <Content>
-            <View>
-                <Text>OffersScreen </Text>
-            </View>
-        </Content>
-        <Footer activeScreen="Offers" />
-    </Container>
-);
+
+const OffersScreen = () => {
+    const [tableNumber, setTableNumber] = useState(null);
+    const [hasPermission, setPermission] = useState(null);
+        
+    useEffect(() => {
+        (async () => {
+          const { status } = await BarCodeScanner.requestPermissionsAsync();
+          setPermission(status === 'granted' ? true : false);
+        })();
+      }, []);
+    
+    const barCodeScanned = ({ data }) => {
+        setTableNumber(data);
+        alert(tableNumber);
+    }
+
+    return ( hasPermission === null ? <Text>Requesting for camera permission</Text> :
+        hasPermission === false ? <Text>No access to camera</Text> :
+        <Container>
+            <Header title="Offers" />
+                    <View  style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'flex-end',
+                    }}
+                    >    
+                        <BarCodeScanner
+                            onBarCodeScanned={!tableNumber ? 'undefined' : barCodeScanned}
+                            style={StyleSheet.absoluteFillObject}
+                        />
+                         {tableNumber && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+                    </View>
+            <Footer activeScreen="Offers" />
+        </Container>
+)};
 
 export default OffersScreen;
+
+
